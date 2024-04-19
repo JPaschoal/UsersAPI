e5cf080e95430b1df924c5557447ef6381ebd2df
Jhonatan Paschoal <jhonatan.paschoal@performait.com>
2024-04-19 00:28:47 -0300

    Add User.cs model class Add appsettings.json configuration file Add UserProfile.cs AutoMapper profile Add UserController.cs controller Add CreateUserDto.cs DTO class Add UsersAPI.csproj project file Add .NETCoreApp,Version=v8.0.AssemblyAttributes.cs file Add .NETCoreApp,Version=v8.0.GlobalUsings.g.cs file Add UsersAPI.sln solution file

---
+++ UsersAPI/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsersAPI.Data.Dtos;
+
+namespace UsersAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult AddUser(CreateUserDto user)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ UsersAPI/Data/Dtos/CreateUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersAPI.Data.Dtos;
+
+public class CreateUserDto
+{
+    [Required]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    public DateTime DataNascimento { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+    [Required]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
